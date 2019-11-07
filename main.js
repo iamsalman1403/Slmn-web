e3e319d047b2a0e6040eae52f309f294f04b954a
iamsalman1403 <39862393+iamsalman1403@users.noreply.github.com>
2019-11-07 11:53:00 +0530

    updated

---
+++ main.js
@@ -0,0 +1,22 @@
+// var slideText = {
+//     distance: '150%',
+//     origin: 'bottom',
+//     opacity: null,
+//     duration: 1200,
+//     delay:100,
+//     easing: 'ease-in',
+// };
+// ScrollReveal().reveal('.bannerText', slideText);
+//
+
+//
+// var slideLeft = {
+//     distance: '200%',
+//     origin: 'left',
+//     opacity: null,
+//     duration: 2000,
+//     delay:100,
+//     easing: 'ease-in',
+// };
+//
+// ScrollReveal().reveal('#revealLeft', slideLeft);
